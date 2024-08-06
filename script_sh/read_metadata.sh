56d67e52f5321b8f40a43bf4bb31bbde872efd78
Sandy Gregorio <sandy@MacBook-Pro-de-Sandy.local>
2024-08-06 10:03:02 -0400

    make a loop over through the GOPRO directories in rename_images.sh

--- script_sh/read_metadata.sh
+++ script_sh/read_metadata.sh
@@ -3,10 +3,10 @@
 # Script to read metadata from images
 
 # Directory containing the images
-image_dir="/Volumes/KH-ISW/Jaeger_Arrow/Fieldwork/20240722/CamDo/20240723-20240724/101GOPRO"
+dir_img="/Volumes/KH-ISW/Jaeger_Arrow/Fieldwork/20240722/CamDo/20240723-20240724/101GOPRO"
 
 # Loop through all images in the directory
-for image in "$image_dir"/*; do
+for image in "$dir_img"/*; do
     echo "Metadata for $image:"
     identify -verbose "$image"
     echo ""
--- script_sh/rename_images.sh
+++ script_sh/rename_images.sh
@@ -4,32 +4,37 @@
 # using the date and time 
 # from their metadata 
 
-# Directory containing the images
-image_dir="/Volumes/KH-ISW/Jaeger_Arrow/Fieldwork/20240722/CamDo/20240727-20240729/102GOPRO"
-image_dir_new="/Volumes/KH-ISW/Jaeger_Arrow/Fieldwork/20240722/CamDo"
+# Directory where to move renamed images
+dir_img_new="/Users/sandy/Documents/ISW_projects/Jaeger_Arrow/Instruments/CamDo/images/20231101"
 
-# Loop through all images in the directory
-for image in "$image_dir"/*; do
-    # Extract the DateTimeOriginal metadata (change this tag if necessary)
-    datetime=$(exiftool -DateTimeOriginal -s3 "$image")
+# Make a loop over the directory containing the raw images
+for dir_img in /Users/sandy/Documents/ISW_projects/Jaeger_Arrow/Instruments/CamDo/images/20231101/*; do
+
+    # Loop through all images in each directory
+    for image in "$dir_img"/*; do
+
+        # Extract the DateTimeOriginal metadata 
+        datetime=$(exiftool -DateTimeOriginal -s3 "$image")
     
-    # Check if the metadata was found
-    if [ -z "$datetime" ]; then
-        echo "No DateTimeOriginal found for $image. Skipping."
-        continue
-    fi
+        # Check if the metadata was found
+        if [ -z "$datetime" ]; then
+            echo "No DateTimeOriginal found for $image. Skipping."
+            continue
+        fi
 
-    # Replace colons and spaces to make a valid filename
-    formatted_datetime=$(echo $datetime | sed 's/://g' | sed 's/ /_/g')
+        # Replace colons and spaces to make a valid filename
+        formatted_datetime=$(echo $datetime | sed 's/://g' | sed 's/ /_/g')
 
-    # Get the file extension
-    extension="${image##*.}"
+        # Get the file extension
+        extension="${image##*.}"
 
-    # Construct the new filename
-    new_filename="${formatted_datetime}.${extension}"
+        # Construct the new filename
+        new_filename="${formatted_datetime}.${extension}"
 
-    # Rename the image
-    mv "$image" "$image_dir_new/$new_filename"
+        # Rename the image
+        mv "$image" "$dir_img_new/$new_filename"
 
-    echo "Renamed $image to $new_filename"
+        echo "Renamed $image to $new_filename"
+    done
+
 done
