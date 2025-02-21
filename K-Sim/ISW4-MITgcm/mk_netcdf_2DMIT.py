410b1ed9792c7d153b7f91cbccfd975edaa96d1c
Sandy Gregorio <sandy@MacBook-Pro-de-Sandy.local>
2025-02-21 11:38:01 -0500

    mk_netcdf_2DMIT.py: correct the sense of propagation of ISW from parallel to wharf to perpendicular to wharf

--- K-Sim/ISW4-MITgcm/mk_netcdf_2DMIT.py
+++ K-Sim/ISW4-MITgcm/mk_netcdf_2DMIT.py
@@ -12,29 +12,28 @@
 ### import modules
 import numpy     as np
 import xarray    as xr
-import netCDF4   as nc
 from scipy.interpolate import interp1d
 
 # horizontal resolution
-dx    = 2 
-# Because my grid along x has a resolution of 0.5m
+dx   = 2 
+# Because my grid along x has a resolution of 0.5 m
 # I need to extract data along x every 4 data
-nx    = 4
+nx   = 4
 # vertical resolution 
-dz    = 2
+dz   = 2
 # timestep 
-dt    = 15
+dt   = 15
 # Because deltaT = 0.5 s, I need to extract my
 # data along the time every 3 data
-nt    = 3
+nt   = 3
 # domain for the wave
 # length
-LISW  = 250
+LISW = 250
 # depth 
-zmax  = -20.25
+zmax = -20.25
 # choose time index to start 
 # just before ISW
-TISW  = 910
+TISW = 910
 
 ### file to get MITgcm simulations
 # root directory
@@ -42,20 +41,22 @@ rdir = '/Users/sandy/Documents/ISW_projects/Jaeger_Arrow/MITgcm_runs/ISW4-CTDF14
 # experiment directory
 edir = 'APE1e5/'
 # grid file
-gMIT = rdir + edir + 'grid.glob.nc'
+gMIT = f"{rdir}{edir}grid.glob.nc" 
 # diagnostics file
-fMIT = rdir + edir + 'dynDiag.APE1e5.nc'
+fMIT = f"{rdir}{edir}dynDiag.{edir[:-1]}.nc" 
 
-### load grid 
-grid = xr.open_dataset(gMIT)
-grid.close()
-
-
-### open netcdf file and load variables
-ds = xr.open_dataset(fMIT)
-# extract u every dt s strating from TISW
-u  = ds.UVELMASS.isel(Y=0).rename({'Zmd000100':'Z'}).assign_coords({"Z":grid.Z.values}).sel(T=slice(TISW,None,nt),Xp1=slice(0,LISW,nx),Z=slice(0,zmax))
-ds.close()
+### load grid
+with xr.open_dataset(gMIT) as grid:
+    Z_values = grid.Z.values
+ 
+### Open dataset and extract u-velocity 
+with xr.open_dataset(fMIT) as ds:
+    u = (
+        ds.UVELMASS.isel(Y=0)
+        .rename({"Zmd000100": "Z"})
+        .assign_coords({"Z": Z_values})
+        .sel(T=slice(TISW, None, nt), Xp1=slice(0, LISW, nx), Z=slice(0, zmax))
+    )
 
 # create z-axis with dz = 2 m 
 zISW  = np.arange(0,-20-dz,-dz)
@@ -71,8 +72,8 @@ for tt in range(len(u)):
 
 # create a 4D matrix with shape (T,Z,Y,X)
 # such as ISW bumps the wharf perpendicularly
-# the wharf is parallel to latitude
-u4d = np.tile(u_i[:, :, np.newaxis, :], (1, 1, len(u.Xp1), 1))
+# the wharf is parallel to longitude
+u4d = np.tile(u_i[:, :, :, np.newaxis], (1, 1, 1, len(u.Xp1)))
 
 ### contruct grid in lat and lon
 ### we want to work in degree
@@ -89,7 +90,7 @@ lon_wharf   = -70.8315
 lat_wharf   =  48.40174
 # resolution in lat and lon
 dlat        = dx/(60*1852)
-dlon        = dlat/np.cos(lat_wharf * np.pi/180)
+dlon        = dlat/np.cos(np.radians(lat_wharf))
 # compute min and max of lon and lat
 lon_min_ISW = lon_wharf - (LISW/(2*dx))*dlon
 lon_max_ISW = lon_wharf + (LISW/(2*dx))*dlon
@@ -100,55 +101,26 @@ lon_ISW     = np.linspace(lon_min_ISW, lon_max_ISW, len(u.Xp1))
 lat_ISW     = np.linspace(lat_min_ISW, lat_max_ISW, len(u.Xp1))
 
 ### save the data in a netcdf file
-### netcdf filename 
-fname  = '/Users/sandy/Documents/ISW_projects/Jaeger_Arrow/K-Sim/netcdf/ISW4-' +edir[:-1]+ '-dxdy' + str(dx) + 'mdz' + str(dz) + 'mdt' + str(dt) + 's.nc'
-#######
-# open netcdf file in write mode
-ds             = nc.Dataset(fname, 'w', format='NETCDF4')
-### create dimensions
-# latitude
-lat_dim         = ds.createDimension('lat', len(u.Xp1)) 
-# longitude
-lon_dim         = ds.createDimension('lon', len(u.Xp1)) 
-# depth
-depth_dim       = ds.createDimension('depth', len(zISW))
-# unlimited axis for time
-time_dim        = ds.createDimension('time', None)
-### create variables
-# latitude
-lat             = ds.createVariable('lat', np.float32, ('lat',))
-lat.units       = 'deg_min_north'
-lat.long_name   = 'latitude' 
-# longitude
-lon             = ds.createVariable('lon', np.float32, ('lon',))
-lon.units       = 'deg_min_west'
-lon.long_name   = 'longitude'
-# depth
-depth           = ds.createVariable('depth', np.float32, ('depth',))
-depth.units     = 'm'
-depth.long_name = 'depth'
-# unlimited axis for time
-time            = ds.createVariable('time', np.float64, ('time',))
-time.units      = 's'
-time.long_name  = 'time' 
-# zonal velocity
-#u               = ds.createVariable('u',np.float64,('time','depth','lat','lon'))
-#u.units         = 'm/s'
-#u.long_name     = 'zonal velocity'
-# meridional velocity
-v               = ds.createVariable('v',np.float64,('time','depth','lat','lon'))
-v.units         = 'm/s'
-v.long_name     = 'meridional velocity'
-### writing data
-lat[:]          = lat_ISW
-lon[:]          = lon_ISW
-depth[:]        = zISW
-time[:]         = np.arange(0,len(u4d)*dt,dt)
-v[:,:,:,:]      = u4d 
-### create attributes
-descrip         = 'Four internal wave propagating perpendicular to the wharf. \
-The horizontal resolution is %d m over a grid of %d m by %d m. \
-The vertical resolution is %d m. The timestep is %d s.'%(dx,LISW,LISW,dz,dt)
-ds.description  = descrip
-### close the Dataset
-ds.close()
+### define output filename
+fname = f"/Users/sandy/Documents/ISW_projects/Jaeger_Arrow/K-Sim/netcdf/ISW4-{edir[:-1]}-dxdy{dx}mdz{dz}mdt{dt}s.nc" 
+### Convert data to xarray Dataset and save to NetCDF
+ds_out = xr.Dataset(
+    {
+        "v": (("time", "depth", "lat", "lon"), u4d),
+    },
+    coords={
+        "lat": lat_ISW,
+        "lon": lon_ISW,
+        "depth": zISW,
+        "time": np.arange(0, len(u4d) * dt, dt),
+    },
+    attrs={
+        "description": f"Four internal waves propagating perpendicular to the wharf. "
+                       f"The horizontal resolution is {dx} m over a grid of {LISW} m by {LISW} m. "
+                       f"The vertical resolution is {dz} m. The timestep is {dt} s."
+    },
+)
+
+# Save using xarray 
+ds_out.to_netcdf(fname)
+
